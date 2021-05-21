f9fcb5675c955468a28177f632ee5e8ffb8dcf1c
Sandeep Reddy <sandeepny441@gmail.com>
2021-05-21 07:02:48 -0500

    added groupby commands

--- main_idea.py
+++ main_idea.py
@@ -1,1 +1,4 @@
 # We will write what we plan to do here in this file.
+# add all pandas commands used for Time Series and in geenral
+# add all R commands as well for Time Series and in general
+# Keep the code snippets used for the Hyndman book. 
