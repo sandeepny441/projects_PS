856caa00f64c008f84d99aede8c4a722ef0ee931
Sandeep Reddy <sandeepny441@gmail.com>
2021-05-21 06:42:01 -0500

    added group by commands

--- ts_pandas_snippets.py
+++ ts_pandas_snippets.py
@@ -1,47 +1,51 @@
 import pandas as pd
 import numpy as np
 
+pd.set_option("display.precision", 3)
+pd.set_option("display.max_rows", 25)
+
 # creation:
-df = pd.DataFrame({"a": [1, 2, 3], "b": [10, 20, 30]}, index=[1000, 2000, 3000])
-df = pd.DataFrame([[1, 2, 3], [10, 20, 30]], index=[40, 41], columns=["a", "b", "c"])
+df = pd.DataFrame({'a': [1, 2, 3], 'b': [10, 20, 30], index=[1000, 2000, 3000]})
+df = pd.DataFrame([[1, 2, 3], [10, 20, 30], index=[40, 41], columns=['a', 'b', 'c']])
 
 # loading_data
-df = pd.read_csv("titanic.csv")
-df = pd.read_csv("titanic.csv", skiprows=1)
-df = pd.read_excel("titanic.excel")
-df = pd.read_html("titanic.html")
+df = pd.read_csv('titanic.csv')
+df = pd.read_csv('titanic.csv', skiprows=1)
+df = pd.read_excel('titanic.excel')
+df = pd.read_html('titanic.html')
 
 # most used:
 df.head()
 df.tail()
 df.sample(5)
 df.sample(frac=0.3)
-df.nlargest(10, "A")
-df.smallest(10, "B")
+df.nlargest(10, 'A')
+df.smallest(10, 'B')
 
 # info:
 df.info()
 df.shape
 df.describe()
 df.dtypes
-df["A"].value_counts()
-df["A"].count()
-df["A"].unique()
+df['A'].value_counts()
+df['A'].count()
+df['A'].unique()
 df.drop_duplicates()
 df.values()
-df.filter(items=["A"])
-df.filter(regex="s$")  # column_name_match
-df.filter(like="san")  # row_name_match
+df.filter(items=['A'])
+df.filter(regex='s$')  # column_name_match
+df.filter(like='san')  # row_name_match
 
 # columns:
 df.columns
-df.rename(columns={"A": "B"})
-df.drop(["A"], inplace=True)
-df.columns = ["A", "B", "C"]
+df.rename(columns={'A': 'B'})
+df.drop(['A'], inplace=True)
+df.columns = ['A', 'B', 'C']
 
 # rows:
 for i, row in df.iterrows():
     print(i, row)
+
 
 # null_values:
 df.isna()
@@ -49,52 +53,58 @@ df.isnull()
 df.dropna()
 df.fillna(10)
 df.interpolate()
-df.interpolate(method="linear", limit_direction="forward")
-df.interpolate(method="linear", limit_direction="backward")
-df.interpolate(method="linear", limit_direction="both")
+df.interpolate(method='linear', limit_direction='forward')
+df.interpolate(method='linear', limit_direction='backward')
+df.interpolate(method='linear', limit_direction='both')
+
+
 # selection:
 df.loc[1:100, 2:4]
-df.loc[1:100, "A"]
-df.loc[1:100, ["A", "B"]]
+df.loc[1:100, 'A']
+df.loc[1:100, ['A', 'B']]
 
-# selectiuon with information at location
+#selectiuon with information at location
 df.iloc[1:100, 2:5]
-df.loc[df["A"] > 100]
-df["A"].loc[lambda x: x > 100]
-df1 = df[["A", "B"]]
+df.loc[df['A'] > 100]
+df['A'].loc[lambda x: x > 100]
+df1 = df[['A', 'B']]
 
-# concat  || Increasing Height
+# concat  || Increasing Height 
 pd.concat([df1, df2], axis=0)
 pd.concat([df1, df2], axis=1)
 
-# merge | joins in SQL:|| Increasing with
+# merge | joins in SQL:|| Increasing with  
 pd.merge(df1, df2)
-pd.merge(df1, df2, how="inner", on="a")
-pd.merge(df1, df2, how="left", on="a")
-pd.merge(df1, df2, how="right", on="a")
-pd.merge(df1, df2, how="outer", on="a")
-# how to include condition like we include where condition in SQL?
+pd.merge(df1, df2, how='inner', on='a')
+pd.merge(df1, df2, how='left', on='a')
+pd.merge(df1, df2, how='right', on='a')
+pd.merge(df1, df2, how='outer', on='a')
+#how to include condition like we include where condition in SQL?
 
 # sort:
-df["A"].sort_values()
-df["A"].sort_values(ascending=False)
+df['A'].sort_values()
+df['A'].sort_values(ascending=Falase)
 df.sort_index()
 
 # index:
 df.reset_index(drop=True)
-df.set_index("month")
-
+df.set_index('month')
 
 # aggragtes:
+ df.groupby("column_a")["column_b"].count()
+ df.groupby(["column_a", "column_b"])["column_c"].count()
 
-
+df.groupby("column_a", sort= True)["column_b"].count()
+df.groupby("column_a", sort= True)["column_b"].count().rename_axis(['column_a'])
+df.groupby("column_a", sort= True)["column_b"].agg(['min', 'max'])
 # groupby:
+
 
 
 # Pivot
 
 
-# apply_function:
+# lambda and apply_function:
 
 
-# lambda:
+
