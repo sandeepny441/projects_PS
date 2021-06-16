34811d8ae88589229b74e45e0975369c1e2a3e07
Sandeep Reddy <sandeepny441@gmail.com>
2021-06-16 13:30:07 -0500

    added python repo

---
+++ numpy_conceptwise/numpy_jake_python_files_s/ufuncs.py
@@ -0,0 +1,6 @@
+import numpy as np 
+np.random.seed(0)  # seed for reproducibility
+
+x1 = np.random.randint(10, size=6)  # One-dimensional array
+x2 = np.random.randint(10, size=(3, 4))  # Two-dimensional array
+x3 = np.random.randint(10, size=(3, 4, 5))  # Three-dimensional array
