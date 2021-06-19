03ba6d6b2ee9528367fa8223af2defb9e2145b56
Sandeep Reddy <sandeepny441@gmail.com>
2021-06-19 13:16:20 -0500

    deleted some unwanted files

--- basic_rock.py
+++
@@ -1,7 +0,0 @@
-a = (lambda x, y: x + y)
-
-
-a(2,3)
-
-
-print(a(2,3))
--- main_idea.py
+++
@@ -1,4 +0,0 @@
-# We will write what we plan to do here in this file.
-# add all pandas commands used for Time Series and in geenral
-# add all R commands as well for Time Series and in general
-# Keep the code snippets used for the Hyndman book. 
